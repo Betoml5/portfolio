5c22e9fd4219aecc476878a36e90c4ea3be5072a
Betoml5 <betoml5@hotmail.com>
2022-02-24 15:44:24 -0600

    Add layout

---
+++ containers/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const Layout = ({ children }) => {
+  <Header />;
+  {
+    children;
+  }
+  <Footer />;
+};
+
+export default Layout;
