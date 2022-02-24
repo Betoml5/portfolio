dddd6353c3497ee1ce32768a1156ae3565d072c1
Betoml5 <betoml5@hotmail.com>
2022-02-24 15:44:44 -0600

    Add progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
+import NextNProgress from "nextjs-progressbar";
 import Layout from "../containers/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <NextNProgress />
       <Component {...pageProps} />
     </Layout>
   );
