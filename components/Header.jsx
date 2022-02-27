21f5f7a654cae7214f1d1f07b1a8c239c8b33231
Betoml5 <betoml5@hotmail.com>
2022-02-27 16:46:05 -0600

    Add projects images

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header className=" bg-primary p-4">
-      <div className="flex items-center justify-between">
+    <header className="relative bg-primary p-4">
+      <div className="flex items-center justify-between ">
         <div className="flex items-center">
           <picture>
             <Image
@@ -17,9 +19,35 @@ const Header = () => {
           </picture>
           <p className="text-white ml-4">Beto Martinez</p>
         </div>
-        <div className="flex text-white">
-          <p className="mr-2">Proyectos</p>
+
+        <div className="hidden text-white md:flex">
+          <a href="#projects" className="mr-2">
+            Proyectos
+          </a>
           <a href="#about">Sobre mi</a>
+        </div>
+
+        <div className="text-white md:hidden">
+          <div onClick={() => setOpen(!open)} className="md:hidden">
+            <Image
+              src="/hamburger.png"
+              width={25}
+              height={25}
+              alt="hamburger"
+            />
+          </div>
+          <div
+            className={`absolute left-0  bg-primary p-4 w-full border-t border-secondary transition-all duration-300 ease-linear md:flex md:fixed ${
+              open
+                ? "-bottom-14 opacity-100"
+                : "bottom-[420px] md:opacity-100 opacity-0 "
+            }`}
+          >
+            <a href="#projects" className="mr-2">
+              Proyectos
+            </a>
+            <a href="#about">Sobre mi</a>
+          </div>
         </div>
       </div>
     </header>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,40 +1,46 @@
 import Image from "next/image";
+import { projects } from "../projects.json";
 
 export default function Home() {
   return (
     <main>
-      <section className="p-4" id="projects">
-        <h2 className="text-2xl text-white">Proyectos</h2>
-        <div className="grid grid-cols-1 gap-2 mt-6">
-          {[1, 2, 3, 4, 5].map((item) => (
-            // eslint-disable-next-line react/jsx-key
-            <div className="flex flex-col  bg-primary text-white p-4 rounded-md">
-              <p className=" mb-4">Project name</p>
-              <Image
-                src="/me.jpeg"
-                width={500}
-                height={500}
-                alt="web"
-                className="h-full w-full"
-              />
-              <p className=" mt-4">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex
-                aliquam ipsam nostrum quia? Nisi voluptate labore nam cum
-                suscipit blanditiis fugit, doloremque aliquid a ea reiciendis
-                quidem repudiandae sequi. Voluptate.
-              </p>
-            </div>
-          ))}
-        </div>
-      </section>
+      <section className="w-3/4 mx-auto">
+        <section className="text-white p-4 mt-14 md:mt-0 ">
+          <h2 className="text-3xl font-semibold">
+            Beto Martinez, Frontend Developer
+          </h2>
+        </section>
 
-      <section className="text-white p-4" id="about">
-        <h2 className="text-2xl mb-2">Acerca de mi</h2>
-        <p className="tracking-widest">
-          Hola, soy Beto Martinez, Frontend developer. Tengo 19 años y vivo en
-          México. Actualmente estoy cursando la carrera de Ingerieria en
-          Sistemas Computaciones en el TECNM Campus Region carbonifera
-        </p>
+        <section className="text-white p-4 " id="about">
+          <h2 className="text-2xl mb-2">Acerca de mi</h2>
+          <p className="tracking-widest">
+            Hola, soy Beto Martinez, Frontend developer. Tengo 19 años y vivo en
+            México. Actualmente estoy cursando la carrera de Ingerieria en
+            Sistemas Computaciones en el TECNM Campus Region carbonifera
+          </p>
+        </section>
+        <section className="p-4" id="projects">
+          <h2 className="text-2xl text-white ">Proyectos</h2>
+          <div className="grid grid-cols-1 gap-4 mt-6 md:grid-cols-2 grid-flow-row-dense">
+            {projects.map((item) => (
+              // eslint-disable-next-line react/jsx-key
+              <div
+                className="flex flex-col  bg-primary text-white p-4 rounded-md cursor-pointer"
+                key={item.id}
+              >
+                <p className=" text-2xl mb-4">{item.name}</p>
+                <Image
+                  src={item.image}
+                  width={400}
+                  height={400}
+                  alt={item.name}
+                  className="h-full w-full object-contain rounded-lg"
+                />
+                <p className="mt-4">{item.description}</p>
+              </div>
+            ))}
+          </div>
+        </section>
       </section>
     </main>
   );
